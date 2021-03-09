8d3307ed9ac2ea090f2798efcd0b3daa59067bb6
Tomas Munro <tomas.k.munro@gmail.com>
2021-03-09 16:38:52 +1100

    added unit test and lip engine

---
+++ MathLibrary/C# Unit Test - Student Copy/MathClasses/Vector3.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClasses
+{
+	public struct Vector3
+	{
+		public float x;
+		public float y;
+		public float z;
+
+		public Vector3(float x, float y, float z)
+		{
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
+
+		public static Vector3 operator +(Vector3 lhs, Vector3 rhs)
+		{
+			Vector3 result;
+			result.x = lhs.x + rhs.x;
+			result.y = lhs.y + rhs.y;
+			result.z = lhs.z + rhs.z;
+			return result;
+
+		}
+
+		public static Vector3 operator -(Vector3 lhs, Vector3 rhs)
+		{
+			Vector3 result;
+			result.x = lhs.x - rhs.x;
+			result.y = lhs.y - rhs.y;
+			result.z = lhs.z - rhs.z;
+			return result;
+
+		}
+		public static Vector3 operator *(Vector3 lhs, float rhs)
+		{
+			Vector3 result;
+			result.x = lhs.x * rhs;
+			result.y = lhs.y * rhs;
+			result.z = lhs.z * rhs;
+			return result;
+
+		}
+		public static Vector3 operator *(float lhs, Vector3 rhs)
+		{
+			Vector3 result;
+			result.x = lhs * rhs.x;
+			result.y = lhs * rhs.y;
+			result.z = lhs * rhs.z;
+			return result;
+
+		}
+		public float Magnitude()
+		{
+			//c= sqrt(a^2 + b^2)
+			return (float)Math.Sqrt((x * x) + (y * y) + (z * z));
+
+		}
+		public void Normalise()
+		{
+			float magnitude = Magnitude();
+			if (magnitude != 0)
+			{
+				x /= magnitude;
+				y /= magnitude;
+				z /= magnitude;
+			}
+		}
+	}
+}
---
+++ MathLibrary/C# Unit Test - Student Copy/MathClasses/Vector4.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClasses
+{
+	public struct Vector4
+	{
+		public float x;
+		public float y;
+		public float z;
+		public float w;
+
+		public Vector4(float x, float y, float z, float w)
+		{
+			this.x = x;
+			this.y = y;
+			this.z = z;
+			this.w = w;
+		}
+
+		public static Vector4 operator +(Vector4 lhs, Vector4 rhs)
+		{
+			Vector4 result;
+			result.x = lhs.x + rhs.x;
+			result.y = lhs.y + rhs.y;
+			result.z = lhs.z + rhs.z;
+			result.w = lhs.w + rhs.w;
+			return result;
+
+		}
+
+		public static Vector4 operator -(Vector4 lhs, Vector4 rhs)
+		{
+			Vector4 result;
+			result.x = lhs.x - rhs.x;
+			result.y = lhs.y - rhs.y;
+			result.z = lhs.z - rhs.z;
+			result.w = lhs.w - rhs.w;
+			return result;
+
+		}
+		public static Vector4 operator *(Vector4 lhs, float rhs)
+		{
+			Vector4 result;
+			result.x = lhs.x * rhs;
+			result.y = lhs.y * rhs;
+			result.z = lhs.z * rhs;
+			result.w = lhs.w * rhs;
+			return result;
+
+		}
+		public static Vector4 operator *(float lhs, Vector4 rhs)
+		{
+			Vector4 result;
+			result.x = lhs * rhs.x;
+			result.y = lhs * rhs.y;
+			result.z = lhs * rhs.z;
+			result.w = lhs * rhs.w;
+			return result;
+
+		}
+		public float Magnitude()
+		{
+			//c= sqrt(a^2 + b^2)
+			return (float)Math.Sqrt((x * x) + (y * y) + (z * z));
+
+		}
+		public void Normalise()
+		{
+			float magnitude = Magnitude();
+			if (magnitude != 0)
+			{
+				x /= magnitude;
+				y /= magnitude;
+				z /= magnitude;
+				w /= magnitude;
+			}
+		}
+
+	}
+}
