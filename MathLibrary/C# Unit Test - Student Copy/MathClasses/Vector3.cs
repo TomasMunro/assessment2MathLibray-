9b8b3900bc53a6007d5810b57a9b173c3a813796
Tomas Munro <tomas.k.munro@gmail.com>
2021-03-10 11:05:21 +1100

    update

--- MathLibrary/C# Unit Test - Student Copy/MathClasses/Vector3.cs
+++ MathLibrary/C# Unit Test - Student Copy/MathClasses/Vector3.cs
@@ -78,7 +78,18 @@ namespace MathClasses
 			return (x * rhs.x) + (y * rhs.y) + (z * rhs.z);
 
 		}
-		//this finds the the right angles of a vector
+		//if you want to find the radius betweent two points
+		//normalize setting both vectors to 1
+		//Dot(find the ratio) between two points
+		//find out which direction= you need to create a right angle
+		//you do that by picking a point lets say A swapping the vector values eg)y=x,x=y
+		//now we have a new point C which is right angle to A
+		//we then perfor a Dot function Betweeen point C and B if result is positive the quickest direction between a-b is positive 
+		//
+
+		//-----------------------------------------------------------------------------------
+		//this finds the the right angles of a vector(facing forward whats right (angle) or right)
+
 		public Vector3 Cross(Vector3 rhs)
 		{
 			Vector3 result;
@@ -89,6 +100,7 @@ namespace MathClasses
 
 
 		}
+		//if you want to find what 
 
 
 
